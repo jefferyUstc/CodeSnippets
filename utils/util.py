7fa98d996995ddf021e3c2a58041e1a5085e744e
jeffery <jeffery_cpu@163.com>
2019-08-10 10:10:09 +0800

    add get_cpu_num && get_process_memory func

--- utils/util.py
+++ utils/util.py
@@ -1,4 +1,6 @@
 import sys
+from psutil import cpu_count, Process
+from multiprocessing import cpu_count
 
 
 def get_version(brief=False):
@@ -6,3 +8,22 @@ def get_version(brief=False):
         return sys.version_info
     return sys.version_info[0]
 
+
+def get_cpu_cores(logical=False):
+    return cpu_count(logical=logical)
+
+
+def get_cpu_cores2(logical=False):
+    return cpu_count(logical=logical)
+
+
+def get_process_memory(pid):
+    if isinstance(pid, int):
+        process = Process(pid)
+    else:
+        raise TypeError('pid should be int type')
+    info = process.memory_info()
+    return {
+        'vms': info.vms,
+        'rss': info.rss
+    }
